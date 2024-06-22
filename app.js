4f916eee9f3d02e9ae49aae7b51be4f909fe78aa
Brandon-Morales-26 <brandonjmm11@gmail.com>
2024-06-21 23:15:30 -0400

    vistas de contactos

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var contactosRouter = require ("./routes/contactos");
+
 // view engine setup
+
+app.use("/contactos" , contactosRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
